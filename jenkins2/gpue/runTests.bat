c7ad01c5947e4c06cfcc95da97d997f109de41aa
foreigner <starka@fit.vutbr.cz>
2017-02-02 15:50:12 +0100

    fix

--- jenkins2/gpue/runTests.bat
+++ jenkins2/gpue/runTests.bat
@@ -1,16 +1,16 @@
 rem @echo off
-rem set WORKSPACE="C:\jenkins_slave\workspace\GPUnGine_tests_msvc2013"
-mkdir %WORKSPACE%\log
+rem set WORKSPACE=C:/jenkins2_slave/workspace/GPUE/GPUEngine_main
+mkdir %WORKSPACE%/log
 echo "flag0"
-del %WORKSPACE%\log\* /Q
+del %WORKSPACE%/log/* /Q
 
-cd %WORKSPACE%\gpuengine-code-build\bin\
+cd %WORKSPACE%/gpuengine-code-build/bin/
 set /a "RET=0"
 
 FOR %%i in (*.exe) do (
-   %%i >> %WORKSPACE%\log\%1out.txt
+   %%i >> %WORKSPACE%/log/%1out.txt
    IF %errorlevel% neq 0 echo test/app %%i exited with error %errorlevel%
    set /a "RET|=%errorlevel%"
 )
 
-exit %RET%
+rem exit %RET%
