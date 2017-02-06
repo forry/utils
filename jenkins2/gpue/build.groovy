6ad7a93514387088eece92be55873349f5f4cc22
foreigner <starka@fit.vutbr.cz>
2017-02-06 11:36:35 +0100

    reformating, debug parameter

--- jenkins2/gpue/build.groovy
+++ jenkins2/gpue/build.groovy
@@ -1,5 +1,5 @@
 #!groovy
-def call(){
+def call(debug = false){
    def result = "FAILED";
    def testMap = [msvc2015:false]
    def messageColorMap = [success:'#439FE0',fail:'#E40000' ,testFail:'#FF9D3C']
@@ -8,47 +8,49 @@ def call(){
    stage('builds')
    {
       node('windows' && 'msvc2015') {
-          try{
-             def repo = 'gpuengine-code'
-             def buildDir = 'gpuengine-code-build'
-             def buildScript = 'build_script/jenkins2/gpue'
-			 def buildPrefix = prefixes['msvc2015']
+         try{
+            def repo = 'gpuengine-code'
+            def buildDir = 'gpuengine-code-build'
+            def buildScript = 'build_script/jenkins2/gpue'
+            def buildPrefix = prefixes['msvc2015']
 			 
-             //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'build_script'], [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'jenkins2/gpue']]]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/forry/utils.git']]])
-             checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: repo]], submoduleCfg: [], userRemoteConfigs: [[url: 'git://git.code.sf.net/p/gpuengine/code']]])
-             def cmake = tool name: 'CMake', type: 'hudson.plugins.cmake.CmakeTool'
-             //clean build dir
-             sh "rm -rf ${buildDir}"
-             sh "mkdir ${buildDir}"
-             dir(buildDir) 
-             {
-               sh "${cmake} -C ../${buildScript}/msvc2015/cache_min.cmake -G \"Visual Studio 14 2015 Win64\" ../${repo}"
-             }
-             def msbuild = tool name: 'MSBUILD4', type: 'hudson.plugins.msbuild.MsBuildInstallation'
-             bat "${msbuild}/msbuild.exe /p:Configuration=release ${buildDir}/ALL_BUILD.vcxproj"
-             
-             def testRet = bat( returnStatus: true, script: "${buildScript}/runTests.bat ${buildPrefix}")
-			 echo " Returned test value $testRet"
-			 testMap['msvc2015'] = testRet == 0;
-             stash includes: "log/${buildPrefix}out.txt", name: 'unit_tests', useDefaultExcludes: false
-             result = "SUCCESS"
-          } catch (e)
-          {
-              result = "FAILED"
-			  echo "MSVC2015 failed!!!!!!!!!!"
-			  printThrowable(e)
-              //throw e
-          }
-          finally
-          {
-              def body = ""
-              def color = "#36A64F"
-              body = "$result Job $JOB_NAME #$BUILD_NUMBER\n ${BUILD_URL}console"
-              if(result != "SUCCESS")
-              { 
-                  color = "#E40000"
-              }
-          }
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'build_script'], [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'jenkins2/gpue']]]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/forry/utils.git']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: repo]], submoduleCfg: [], userRemoteConfigs: [[url: 'git://git.code.sf.net/p/gpuengine/code']]])
+            def cmake = tool name: 'CMake', type: 'hudson.plugins.cmake.CmakeTool'
+            //clean build dir
+            sh "rm -rf ${buildDir}"
+            sh "mkdir ${buildDir}"
+            dir(buildDir) 
+            {
+            sh "${cmake} -C ../${buildScript}/msvc2015/cache_min.cmake -G \"Visual Studio 14 2015 Win64\" ../${repo}"
+            }
+            def msbuild = tool name: 'MSBUILD4', type: 'hudson.plugins.msbuild.MsBuildInstallation'
+            bat "${msbuild}/msbuild.exe /p:Configuration=release ${buildDir}/ALL_BUILD.vcxproj"
+         
+            result = "SUCCESS"
+            def testRet = bat( returnStatus: true, script: "${buildScript}/runTests.bat ${buildPrefix}")
+            if(debug)
+               echo " Returned test value $testRet"
+            testMap['msvc2015'] = testRet == 0;
+            stash includes: "log/${buildPrefix}out.txt", name: 'unit_tests', useDefaultExcludes: false
+         } catch (e)
+         {
+            result = "FAILED"
+            if(debug)
+               echo "MSVC2015 failed!!!!!!!!!!"
+			   printThrowable(e)
+            //throw e
+         }
+         finally
+         {
+            def body = ""
+            def color = "#36A64F"
+            body = "$result Job $JOB_NAME #$BUILD_NUMBER\n ${BUILD_URL}console"
+            if(result != "SUCCESS")
+            { 
+               color = "#E40000"
+            }              
+         }
          
       }
    }
@@ -56,10 +58,10 @@ def call(){
    {
       node('master')
       {
-		  def attachment = ""
-		  def testssubject = "SUCCEEDED"
-		  def failedTestMessage = "";
-		  testMap.each{ key, value ->
+         def attachment = ""
+         def testssubject = "SUCCEEDED"
+         def failedTestMessage = "";
+         testMap.each{ key, value ->
 			if(!value)
 			{
 				failedTestMessage += "$key test has FAILED!\n"
@@ -70,16 +72,19 @@ def call(){
 		try{
           unstash 'unit_tests'
 		} catch (e) { attachment = ""}
-		  def slackMessageColor = result == "SUCCESS"? testssubject == "SUCCEEDED" ? messageColorMap['success'] : messageColorMap['testFail'] : messageColorMap['fail']
-		  def subject = "$JOB_NAME - Build # $BUILD_NUMBER - $result!, Tests : $testssubject"
-          def mailbody = "$JOB_NAME - Build # $BUILD_NUMBER - $result, Tests : $testssubject:\n\nCheck console output at $BUILD_URL to view the results.\n"
-		  def slackMessage = subject + "\n Check console output at $BUILD_URL to view the results."
-		  echo "subject: $subject"
-		  echo "body:\n $mailbody"
-		  echo "attachement: $attachment"
-		  echo "color: $slackMessageColor"
-		  slackSend color: slackMessageColor, message: slackMessage
-          emailext attachLog: true, body: mailbody, subject: subject, to: env.geRecipients, from: 'jenkins', attachmentsPattern: attachment
+         def slackMessageColor = result == "SUCCESS"? testssubject == "SUCCEEDED" ? messageColorMap['success'] : messageColorMap['testFail'] : messageColorMap['fail']
+         def subject = "$JOB_NAME - Build # $BUILD_NUMBER - $result!, Tests : $testssubject"
+         def mailbody = "$JOB_NAME - Build # $BUILD_NUMBER - $result, Tests : $testssubject:\n\nCheck console output at $BUILD_URL to view the results.\n"
+         def slackMessage = subject + "\n Check console output at $BUILD_URL to view the results."
+         if(debug)
+         {
+            echo "subject: $subject"
+            echo "body:\n $mailbody"
+            echo "attachement: $attachment"
+            echo "color: $slackMessageColor"
+         }
+         slackSend color: slackMessageColor, message: slackMessage
+         emailext attachLog: true, body: mailbody, subject: subject, to: env.geRecipients, from: 'jenkins', attachmentsPattern: attachment
       }
    }
 }
